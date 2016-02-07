675204b98e92037a42c77fc59a0cf0191c9cd1a8
santipawar <santipawar@gmail.com>
2016-02-07 17:53:36 -0500

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,15 @@
+## Load and select data subset
+library(data.table)
+d <- fread("household_power_consumption.txt")
+d$Date <- as.Date(d$Date, "%d/%m/%Y")
+d <- d[d$Date >= as.Date("2007-02-01") & d$Date<=as.Date("2007-02-02"), ]
+numcols <- c(3:dim(d)[2])
+d[, (numcols) := lapply(.SD, as.numeric), .SDcols = numcols]
+
+## make plot1.png
+png(filename="plot1.png", width=480, height=480)
+hist(d$Global_active_power,
+     main="Global Active Power",
+     xlab="Global Active Power (kilowatts)",
+     col="red")
+dev.off()
